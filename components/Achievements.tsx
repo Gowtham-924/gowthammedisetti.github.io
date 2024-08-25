b4a0ecc3612bdd6e871cedfd793f27f1fda94b70
Gowtham Medisetti <131621612+Gowtham-924@users.noreply.github.com>
2024-08-25 18:00:13 -0500

    fixed issues

--- components/Achievements.tsx
+++ components/Achievements.tsx
@@ -6,7 +6,7 @@ const Achievements = () => (
       <h2 className={styles.sectionTitle}>Achievements</h2>
       <ul>
         <li>Oracle Cloud Infrastructure 2024 Generative AI Professional Certification</li>
-        <li>First author of the 'LitAI' paper accepted at the 2024 IEEE MIPR Conference</li>
+        <li>First author of the &apos;LitAI&apos; paper accepted at the 2024 IEEE MIPR Conference</li>
         <li>Developed a cutting-edge generative model for academic text generation</li>
         <li>Academy Accreditation - Generative AI Fundamentals from Databricks</li>
         <li>Top contributor to AI-driven projects at Phenom Pvt. Ltd.</li>
--- components/Projects.tsx
+++ components/Projects.tsx
@@ -1,3 +1,4 @@
+import Image from 'next/image';
 import styles from '../styles/Home.module.scss';
 
 const Projects = () => (
@@ -6,12 +7,24 @@ const Projects = () => (
       <h2 className={styles.sectionTitle}>Projects</h2>
       <div className={styles.projectList}>
         <div className={styles.projectItem} data-aos="zoom-in">
-          <img src="/images/project1.png" alt="Project 1" className={styles.projectImg} />
+          <Image 
+            src="/images/project1.png" 
+            alt="Project 1" 
+            className={styles.projectImg} 
+            width={400} // Adjust the width to the appropriate size
+            height={300} // Adjust the height to the appropriate size
+          />
           <h3>LLM-Powered Text Generation for Academic Papers</h3>
           <p>Developed a state-of-the-art generative model using LLAMA, automating content generation for academic papers, which significantly reduced research time.</p>
         </div>
         <div className={styles.projectItem} data-aos="zoom-in">
-          <img src="/images/project1.png" alt="Project 2" className={styles.projectImg} />
+          <Image 
+            src="/images/project2.png" 
+            alt="Project 2" 
+            className={styles.projectImg} 
+            width={400} // Adjust the width to the appropriate size
+            height={300} // Adjust the height to the appropriate size
+          />
           <h3>Generative Adversarial Networks for Fashion Trend Prediction</h3>
           <p>Created a GAN model to analyze and predict future fashion trends, utilizing historical data, in collaboration with Indiana University.</p>
         </div>
